17e3d6592ee6ef373b4029a5fcea859420efc164
Juliana <julianadepaula001@gmail.com>
2022-05-07 17:14:19 -0300

    merge

--- src/pages/CenterMedical/CreateCenterMedical/index.jsx
+++ src/pages/CenterMedical/CreateCenterMedical/index.jsx
@@ -7,7 +7,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import axios from "axios";
 import schema from "./schema";
 import { cepMask } from "../../../utils/cepMask";
-import Sidebar from '../../../Components/Sidebar';
+import Sidebar from "../../../Components/Sidebar";
 
 export default function CreateUser() {
   const {
@@ -51,166 +51,171 @@ export default function CreateUser() {
   };
 
   return (
-    <div className="create-user">
-      <div className="wrapper">
-        <header>Cadastrar novo usuário</header>
-        <form onSubmit={handleSubmit(submitForm)}>
-          <div>
-            <p>Nome</p>
-            <input type="text" name="name" {...register("name")} />
-            <p className="validationError">
-              {" "}
-              {errors?.name && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+    <div>
+      <Sidebar />
+      <div className="create-user">
+        <div className="wrapper">
+          <header>Cadastrar novo centro médico</header>
+          <form onSubmit={handleSubmit(submitForm)}>
+            <div>
+              <p>Nome</p>
+              <input type="text" name="name" {...register("name")} />
+              <p className="validationError">
+                {" "}
+                {errors?.name && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Imagem</p>
-            <input type="text" {...register("image")} />
-            <p className="validationError">
-              {" "}
-              {errors?.image && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Imagem</p>
+              <input type="text" {...register("image")} />
+              <p className="validationError">
+                {" "}
+                {errors?.image && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Telefone</p>
-            <input type="text" {...register("phone")} />
-            <p className="validationError">
-              {" "}
-              {errors?.phone && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Telefone</p>
+              <input type="text" {...register("phone")} />
+              <p className="validationError">
+                {" "}
+                {errors?.phone && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Latitude</p>
-            <input type="text" {...register("latitude")} />
-            <p className="validationError">
-              {" "}
-              {errors?.latitude && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Latitude</p>
+              <input type="text" {...register("latitude")} />
+              <p className="validationError">
+                {" "}
+                {errors?.latitude && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Longitude</p>
-            <input type="text" {...register("longitude")} />
-            <p className="validationError">
-              {" "}
-              {errors?.longitude && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Longitude</p>
+              <input type="text" {...register("longitude")} />
+              <p className="validationError">
+                {" "}
+                {errors?.longitude && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>CEP</p>
-            <input
-              type="text"
-              name="cep"
-              {...register("cep")}
-              onBlur={verifyCEP}
-              onChange={(event) => cepMask(event)}
-            />
-            <p className="validationError">
-              {" "}
-              {errors?.cep && "Insira um CEP válido"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>CEP</p>
+              <input
+                type="text"
+                name="cep"
+                {...register("cep")}
+                onBlur={verifyCEP}
+                onChange={(event) => cepMask(event)}
+              />
+              <p className="validationError">
+                {" "}
+                {errors?.cep && "Insira um CEP válido"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Rua</p>
-            <input type="text" name="street" {...register("street")} />
-            <p className="validationError">
-              {" "}
-              {errors?.street && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Rua</p>
+              <input type="text" name="street" {...register("street")} />
+              <p className="validationError">
+                {" "}
+                {errors?.street && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Número</p>
-            <input type="text" name="number" {...register("number")} />
-            <p className="validationError">
-              {" "}
-              {errors?.number &&
-                "Campo obrigatório, insira apenas números."}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Número</p>
+              <input type="text" name="number" {...register("number")} />
+              <p className="validationError">
+                {" "}
+                {errors?.number &&
+                  "Campo obrigatório, insira apenas números."}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Bairro</p>
-            <input type="text" name="district" {...register("district")} />
-            <p className="validationError">
-              {" "}
-              {errors?.district && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Bairro</p>
+              <input type="text" name="district" {...register("district")} />
+              <p className="validationError">
+                {" "}
+                {errors?.district && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div>
-            <p>Cidade</p>
-            <input type="text" name="city" {...register("city")} />
-            <p className="validationError">
-              {" "}
-              {errors?.city && "Campo obrigatório"}{" "}
-            </p>
-          </div>
+            <div>
+              <p>Cidade</p>
+              <input type="text" name="city" {...register("city")} />
+              <p className="validationError">
+                {" "}
+                {errors?.city && "Campo obrigatório"}{" "}
+              </p>
+            </div>
 
-          <div className="drop-list">
-            <div className="from">
-              <p>Estado</p>
-              <div className="select-box">
-                <select {...register("uf")}>
-                  <option value={"Acre (AC)"}>Acre (AC) </option>
-                  <option value={"Alagoas (AL)"}>Alagoas (AL)</option>
-                  <option value={"Amazonas (AM)"}>Amazonas (AM)</option>
-                  <option value={"Bahia (BA)"}> Bahia (BA)</option>
-                  <option value={"Ceará (CE)"}> Ceará (CE)</option>
-                  <option value={"Distrito Federal (DF)"}>
-                    Distrito Federal (DF)
-                  </option>
-                  <option value={"Espírito Santo (ES)"}>
-                    Espírito Santo (ES)
-                  </option>
-                  <option value={"Goiás	(GO)"}>Goiás (GO)</option>
-                  <option value={"Maranhão	(MA)"}>Maranhão (MA) </option>
-                  <option value={"Mato Grosso	(MT)"}> Mato Grosso (MT)</option>
-                  <option value={" Mato Grosso do Sul	(MS)"}>
+            <div className="drop-list">
+              <div className="from">
+                <p>Estado</p>
+                <div className="select-box">
+                  <select {...register("uf")}>
+                    <option value={"Acre (AC)"}>Acre (AC) </option>
+                    <option value={"Alagoas (AL)"}>Alagoas (AL)</option>
+                    <option value={"Amazonas (AM)"}>Amazonas (AM)</option>
+                    <option value={"Bahia (BA)"}> Bahia (BA)</option>
+                    <option value={"Ceará (CE)"}> Ceará (CE)</option>
+                    <option value={"Distrito Federal (DF)"}>
+                      Distrito Federal (DF)
+                    </option>
+                    <option value={"Espírito Santo (ES)"}>
+                      Espírito Santo (ES)
+                    </option>
+                    <option value={"Goiás	(GO)"}>Goiás (GO)</option>
+                    <option value={"Maranhão	(MA)"}>Maranhão (MA) </option>
+                    <option value={"Mato Grosso	(MT)"}> Mato Grosso (MT)</option>
+                    <option value={" Mato Grosso do Sul	(MS)"}>
+                      {" "}
+                      Mato Grosso do Sul (MS)
+                    </option>
+                    <option value={"Minas Gerais	(MG)"}>
+                      Minas Gerais (MG){" "}
+                    </option>
+                    <option value={"Pará	(PA)"}>Pará (PA) </option>
+                    <option value={" Paraíba (PB)"}> Paraíba (PB) </option>
+                    <option value={"Paraná (PR)"}>Paraná (PR)</option>
+                    <option value={"Pernambuco	(PE)"}> Pernambuco (PE)</option>
+                    <option value={"Piauí	(PI)"}> Piauí (PI)</option>
+                    <option value={"Rio de Janeiro (RJ)"}>
+                      {" "}
+                      Rio de Janeiro (RJ)
+                    </option>
+                    <option value={"Rio Grande do Norte	(RN)"}>
+                      Rio Grande do Norte (RN){" "}
+                    </option>
+                    <option value={" Rio Grande do Sul 	(RS)"}>
+                      {" "}
+                      Rio Grande do Sul (RS){" "}
+                    </option>
+                    <option value={"Rondônia	(RO)"}>Rondônia (RO) </option>
+                    <option value={"Roraima	(RR)"}> Roraima (RR)</option>
+                    <option value={"Santa Catarina (SC)"}>
+                      {" "}
+                      Santa Catarina (SC)
+                    </option>
+                    <option value={"São Paulo (SP)"}> São Paulo (SP)</option>
+                    <option value={"Sergipe	(SE)"}> Sergipe (SE)</option>
+                    <option value={"Tocantins	(TO)"}>Tocantins (TO)</option>
+                  </select>
+                  <p className="validationError">
                     {" "}
-                    Mato Grosso do Sul (MS)
-                  </option>
-                  <option value={"Minas Gerais	(MG)"}>Minas Gerais (MG) </option>
-                  <option value={"Pará	(PA)"}>Pará (PA) </option>
-                  <option value={" Paraíba (PB)"}> Paraíba (PB) </option>
-                  <option value={"Paraná (PR)"}>Paraná (PR)</option>
-                  <option value={"Pernambuco	(PE)"}> Pernambuco (PE)</option>
-                  <option value={"Piauí	(PI)"}> Piauí (PI)</option>
-                  <option value={"Rio de Janeiro (RJ)"}>
-                    {" "}
-                    Rio de Janeiro (RJ)
-                  </option>
-                  <option value={"Rio Grande do Norte	(RN)"}>
-                    Rio Grande do Norte (RN){" "}
-                  </option>
-                  <option value={" Rio Grande do Sul 	(RS)"}>
-                    {" "}
-                    Rio Grande do Sul (RS){" "}
-                  </option>
-                  <option value={"Rondônia	(RO)"}>Rondônia (RO) </option>
-                  <option value={"Roraima	(RR)"}> Roraima (RR)</option>
-                  <option value={"Santa Catarina (SC)"}>
-                    {" "}
-                    Santa Catarina (SC)
-                  </option>
-                  <option value={"São Paulo (SP)"}> São Paulo (SP)</option>
-                  <option value={"Sergipe	(SE)"}> Sergipe (SE)</option>
-                  <option value={"Tocantins	(TO)"}>Tocantins (TO)</option>
-                </select>
-                <p className="validationError">
-                  {" "}
-                  {errors?.uf && "Campo obrigatório"}{" "}
-                </p>
+                    {errors?.uf && "Campo obrigatório"}{" "}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <button>ADICIONAR CENTRO MÉDICO</button>
-        </form>
+            <button>ADICIONAR CENTRO MÉDICO</button>
+          </form>
+        </div>
       </div>
     </div>
   );
